7d0ff59e5ce7cc33fb00c699d3818393addf908c
Drewpew <agcummings11@gmail.com>
2016-04-21 07:43:11 -0700

    some change

--- src/Utitlity/InletList.java
+++ src/Utitlity/InletList.java
@@ -5,14 +5,20 @@ import java.util.ArrayList;
 import Inlets.Inlet;
 
 public class InletList{
-	private ArrayList<?> inlets;
+	private ArrayList<Inlet> inlets;
+	private Class wantedClass;
 	
-	public <E extends Inlet> InletList(){
-		inlets = new ArrayList<E>();
+	public InletList(Class<? extends Inlet> _wantedClass){
+		inlets = new ArrayList<Inlet>();
+		wantedClass = _wantedClass;
 	}
 	
 	public boolean add(Inlet obj){
 		inlets.add(obj);
+	}
+	
+	private boolean isCorrectType(Inlet obj){
+		return obj.getClass().equals(wantedClass.toString());
 	}
 	
 }
